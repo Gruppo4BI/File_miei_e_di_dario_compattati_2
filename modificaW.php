3f53c62998f647e0ad5e873c33d28c835e9361b2
dariotr <gaskdfire@gmail.com>
2016-04-05 15:30:45 +0200

    Update modificaW.php
    Ho modificato tutta la form per usare bootstrap

--- modificaW.php
+++ modificaW.php
@@ -95,54 +95,82 @@ $dati_tel=$result_tel->fetch_assoc();
       <div class="well well-lg">
         <h4>Modifica i tuoi dati</h4>
            <!-- Questo  un form con tutti i dati che sarˆ possibile modificare, utilizzeremo il metodo post -->
-      	 <form  method="post" name="registra" action="emodifica.php" id="registra">
-	<table width="300" >
-			<tr>
-				<td> Categoria</td> <td> <select class="form-control"  name='elencoCategorie' >
+  <form method="post" name="registra" action="emodifica.php" id="registra">
+ 
+  <fieldset class="form-group">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="email" name="email" value='<?php echo $dati["email"]; ?>' >
+  </fieldset>
+  <fieldset class="form-group">
+    <label for="cap">Cap</label>
+    <input type="text" class="form-control" id="cap" name="cap" value='<?php echo $dati["cap"]; ?>'>
+  </fieldset>
+  
+  <fieldset class="form-group">
+    <label for="indirizzo">Indirizzo</label>
+    <input type="indirizzo" class="form-control" id="indirizzo" name="indirizzo" value='<?php echo $dati["indirizzo"]; ?>' >
+  </fieldset>
+  
+  <fieldset class="form-group">
+    <label for="citta">Citta</label>
+    <input type="text" class="form-control" id="citta" name="citta" value='<?php echo $dati["citta"]; ?>'>
+  </fieldset>
+  
+    <fieldset class="form-group">
+    <label for="p_ivaOcf">Partita Iva o Cod Fiscale</label>
+    <input type="text" class="form-control" id="p_ivaOcf" name="p_ivaOcf" value='<?php echo $dati["p_ivaOcf"]; ?>'>
+    </fieldset>
+    
+   <fieldset class="form-group">
+   <label for="sito_web">Sito </label>
+    <input type="text" class="form-control" id="sito_web" name="sito_web" value='<?php echo $dati["sito_web"]; ?>'>
+  </fieldset>
+  
+  
+   <fieldset class="form-group">
+    <label for="numero">Telefono </label>
+    <input type="text" class="form-control" id="numero" name="numero" value='<?php echo $dati_tel["numero"]; ?>'>
+  </fieldset>
+  
+    <fieldset class="form-group">
+    <label for="numero2">Telefono 2 </label>
+    <input type="text" class="form-control" id="numero2" name="numero2" value='<?php echo $dati_tel["numero2"]; ?>'>
+  </fieldset>
+  
+     <fieldset class="form-group">
+    <label for="fax">Fax </label>
+    <input type="text" class="form-control" id="fax" name="fax" value='<?php echo $dati_tel["fax"]; ?>'>
+  </fieldset>
+  
+       <fieldset class="form-group">
+    <label for="cellulare">Cellulare</label>
+    <input type="text" class="form-control" id="cellulare" name="cellulare" value='<?php echo $dati_tel["cellulare"]; ?>'>
+  </fieldset>
+  
+  <fieldset class="form-group">
+    <label for="elencoCategorie">Categoria</label>
+  <select class="form-control"  name='elencoCategorie' >
 												<option  value="<?php  echo $categoria ?>" selected><?php  echo $dati_cat['categoria'];?> </option>
 											<?php 			
 													while	($row = mysqli_fetch_assoc($result) ){
 														echo"<option id='$row[idCat]'>$row[categoria]</option>";
 													}
 											?>		
-												</select> </td>
-			</tr>	
-			<tr>
-				<td> Email</td> <td> <input type="text" id="email" name="email" value='<?php echo $dati["email"]; ?>'> </td>
-			</tr>	
-				<tr>
-				<td> Cap</td> <td> <input type="text" id="cap" name="cap" value='<?php echo $dati["cap"]; ?>'> </td>
-			</tr>	
-				<tr>
-				<td> Indirizzo</td> <td> <input type="text" id="indirizzo" name="indirizzo" value='<?php echo $dati["indirizzo"]; ?>'> </td>
-			</tr>		
-			<tr>
-				<td> Citta</td> <td><input type="text" id="citta" name="citta" value='<?php echo $dati["citta"]; ?>' > </td>
-			</tr>		
-			<tr>
-				<td> Iva o codice fiscale</td> <td> <input type="text" id="p_ivaOcf" name="p_ivaOcf" value='<?php echo $dati["p_ivaOcf"]; ?>'> </td>
-			</tr>
-			<tr>
-				<td> Sito</td> <td><input type="text" id="sito_web" name="sito_web" value='<?php echo $dati["sito_web"]; ?>' > </td>
-			</tr>
-			<tr>
-				<td> Telefono</td> <td><input type="text" id="numero" name="numero" value='<?php echo $dati_tel["numero"]; ?>' > </td>
-			</tr>
-			<tr>
-				<td> Telefono 2</td> <td><input type="text" id="numero2" name="numero2" value='<?php echo $dati_tel["numero2"]; ?>' > </td>
-			</tr>
-			<tr>
-				<td> Fax</td> <td><input type="text" id="fax" name="fax" value='<?php echo $dati_tel["fax"]; ?>' > </td>
-			</tr>
-			<tr>
-				<td>Cellulare</td> <td><input type="text" id="cellulare" name="cellulare" value='<?php echo $dati_tel["cellulare"]; ?>' > </td>
-			</tr>
-			<tr> 
-				<td> Maggiori dettagli </td> <td> <input type="longtext" name="parlaci" value='<?php echo $dati["parlaci"]; ?>' > </td>
-	     	<tr>
-			<td height="100"><input id="aggiorna" value="AGGIORNA" type="submit" name="aggiorna"></td>
-	    	</tr>
-	</table>
+												</select>
+  </fieldset>
+  
+  <fieldset class="form-group">
+    <label for="parlaci">Maggiori dettagli</label>
+    <textarea class="form-control" id="parlaci" name="parlaci" rows="3"> <?php echo $dati["parlaci"]; ?> </textarea>
+  </fieldset>
+  
+<!-- questo potrà essere aggiunto dopo per caricare l immagine dell azienda 
+  <fieldset class="form-group">
+    <label for="exampleInputFile">File input</label>
+    <input type="file" class="form-control-file" id="exampleInputFile">
+    <small class="text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
+  </fieldset> -->
+  <input id="aggiorna" value="AGGIORNA" type="submit" name="aggiorna"></td>
 </form>
 
 <?php 
